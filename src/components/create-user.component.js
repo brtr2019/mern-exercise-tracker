7d276529ca0e0401b765c9ea4b158c5635003f8d
Bradulov Igor Vitalie <igor.bradulov@asp.gov.md>
2020-02-21 16:42:16 +0200

    Frontend,react,components

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+export default class CreateUser extends Component{
+	render(){
+		return (
+			<div>
+				<p>You are in Edit CreateUser list component!!!</p>
+			</div>
+		)
+	}
+}
